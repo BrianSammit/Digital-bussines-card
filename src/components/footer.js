694c8eff973d96c6cc4b9688f28100bae3c74d15
BrianSammit <briansammit.cruz@gmail.com>
2022-05-20 12:11:55 -0500

    Add a basic style

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import { FaTwitterSquare, FaFacebookSquare, FaInstagramSquare, FaGithubSquare } from 'react-icons/fa'
+
+export default function Footer () {
+  return (
+    <div className="footer">
+      <FaTwitterSquare />
+      <FaFacebookSquare />
+      <FaInstagramSquare />
+      <FaGithubSquare /> 
+      
+    </div>
+  ) 
+}

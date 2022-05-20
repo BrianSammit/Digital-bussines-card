9cdb0eea3a75ce974c00607e272846064b60d969
BrianSammit <briansammit.cruz@gmail.com>
2022-05-20 13:53:41 -0500

    Style info component

--- src/components/info.js
+++ src/components/info.js
@@ -1,5 +1,6 @@
 import profilePhoto from "../assets/img/profile_img.jpeg"
 import { FaEnvelope, FaLinkedin } from 'react-icons/fa'
+import './info.css'
 
 export default function Info(){
  return (
@@ -8,9 +9,9 @@ export default function Info(){
       <h1>Brian Sammit Cruz Rodriguez</h1>
       <h3>Front end developer</h3>
       <p>briansammit.cruz@gmail.com</p>
-      <div>
-        <button> <FaEnvelope /> Email</button>
-        <button> <FaLinkedin /> Linkedin</button>
+      <div className="info--buttons">
+        <button className="info--button_email"> <FaEnvelope /> Email</button>
+        <button className="info--button_linkedin"> <FaLinkedin /> Linkedin</button>
       </div>
     </div> 
  ) 
